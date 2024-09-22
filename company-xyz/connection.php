22ef2c920168ed33274181f9d8a1e1b461e50d04
Starcat01 <sfmsx754@gmail.com>
2024-09-23 03:57:23 +0900

    Update connection.php

--- company-xyz/connection.php
+++ company-xyz/connection.php
@@ -7,6 +7,7 @@ function connection()
     $db_name = "company_xyz";
     // create a connection to our database
     $conn = new mysqli($server_name, $username, $password, $db_name);
+    // $conn = mysqli_connect($server_name, $username, $password, $db_name);
     // check if connection is successful
     if ($conn->connect_error) {
         die("Connection Failed. " . $conn->connect_error);
